d7b831e8b455e96cdb779c632b7f2ac4a7ce398d
ttf248 <ttf248@users.noreply.github.com>
2023-09-18 22:21:40 +0000

    deploy: ttf248/notebook@0b9c1d1f8f6e599d73ef19a54081eefc3d73fe96

--- js/copy-code-button.js
+++ js/copy-code-button.js
@@ -1,30 +1,45 @@
 function addCopyButtons(clipboard) {
+    // 遍历所有<pre>内的<code>元素
     document.querySelectorAll("pre > code").forEach(function(codeBlock) {
+        // 创建一个按钮元素
         var button = document.createElement("button");
-        button.className = "copy-code-button";
-        button.type = "button";
-        button.innerText = "Copy";
+        button.className = "copy-code-button"; // 设置按钮的类名
+        button.type = "button"; // 按钮类型为"button"
+        button.innerText = "Copy"; // 按钮上显示的文本为"Copy"
+
+        // 添加点击事件监听器
         button.addEventListener("click", function() {
+            // 复制代码块的文本内容到剪贴板
             clipboard.writeText(codeBlock.textContent).then(function() {
-                button.blur();
-                button.innerText = "Copied!";
+                button.blur(); // 复制完成后移除按钮的焦点
+                button.innerText = "Copied!"; // 按钮文本更改为"Copied!"
                 setTimeout(function() {
-                    button.innerText = "Copy"
-                }, 2e3)
+                    button.innerText = "Copy"; // 2秒后将按钮文本还原为"Copy"
+                }, 2000); // 2秒的延迟
             }, function(error) {
-                button.innerText = "Error";
-                console.error(error)
-            })
+                button.innerText = "Error"; // 如果复制出现错误，将按钮文本设置为"Error"
+                console.error(error); // 在控制台打印错误信息
+            });
         });
+
+        // 获取代码块的父节点<pre>
         var pre = codeBlock.parentNode;
+
+        // 检查代码块是否位于包含类名"highlight"的父元素内
         if (pre.parentNode.classList.contains("highlight")) {
-            var highlight = pre.parentNode;
-            highlight.parentNode.insertBefore(button, highlight)
+            // 确保在同一个 div 内部
+            var highlight = pre;
+            // 创建一个自定义的insertAfter函数插入按钮到highlight之后
+            function insertAfter(newNode, referenceNode) {
+                referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+            }
+            insertAfter(button, highlight);
         } else {
-            pre.parentNode.insertBefore(button, pre)
+            pre.parentNode.insertBefore(button, pre); // 在<pre>之前插入按钮
         }
-    })
+    });
 }
+
 
 var script = document.createElement("script");
 script.src = "https://cdnjs.cloudflare.com/ajax/libs/clipboard-polyfill/2.7.0/clipboard-polyfill.promise.js";
